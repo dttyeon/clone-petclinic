af84e9cd1569701394efc84caaaee75768db79f8
ayeon <dkdus591@gmail.com>
2025-07-15 17:43:05 +0900

    fix

--- src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
@@ -23,8 +23,8 @@ import org.springframework.web.servlet.support.AbstractDispatcherServletInitiali
 
 import javax.servlet.Filter;
 import javax.servlet.ServletContext;
-
-
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import javax.servlet.ServletRegistration;
 /**
  * In Servlet 3.0+ environments, this class replaces the traditional {@code web.xml}-based approach in order to configure the
  * {@link ServletContext} programmatically.
