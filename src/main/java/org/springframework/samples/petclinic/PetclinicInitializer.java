c9854084173bd5679d3257c4a250c0e07a109064
ayeon <dkdus591@gmail.com>
2025-07-16 14:35:28 +0900

    erase exporter

--- src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
@@ -23,9 +23,7 @@ import org.springframework.web.servlet.support.AbstractDispatcherServletInitiali
 
 import javax.servlet.Filter;
 import javax.servlet.ServletContext;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
-import javax.servlet.ServletRegistration;
-import javax.servlet.ServletException;
+
 /**
  * In Servlet 3.0+ environments, this class replaces the traditional {@code web.xml}-based approach in order to configure the
  * {@link ServletContext} programmatically.
@@ -77,12 +75,6 @@ public class PetclinicInitializer extends AbstractDispatcherServletInitializer {
         // Used to provide the ability to enter Chinese characters inside the Owner Form
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter("UTF-8", true);
         return new Filter[]{characterEncodingFilter};
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        super.onStartup(servletContext);
-        servletContext.addListener(new PrometheusMetricsServletContextListener());
     }
 
 }
